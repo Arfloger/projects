79609807f5826a61bb695e40ad20be3a0be4ac18
ksenia <arfloger@gmail.com>
2019-11-05 20:38:43 +0300

    add race

---
+++ game-race/js/main.js
@@ -0,0 +1,136 @@
+const score = document.querySelector(`.score`);
+const start = document.querySelector(`.start`);
+const gameArea = document.querySelector(`.game-area`);
+const car = document.createElement(`div`);
+
+car.classList.add(`car`);
+
+start.addEventListener(`click`, startGame);
+document.addEventListener(`keydown`, startRun);
+document.addEventListener(`keyup`, stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false,
+}
+
+const settings = {
+    start: false,
+    score: 0,
+    speed: 3,
+    traffic: 3,
+}
+
+function getQuantityElements(heightLine) {
+    return document.documentElement.clientHeight / heightLine + 1;
+}
+
+function startGame() {
+    start.classList.add(`hide`);
+    gameArea.innerHTML = ``;
+
+    for(let i = 0; i < getQuantityElements(100); i++) {
+        const line = document.createElement(`div`);
+        line.classList.add(`line`);
+        line.style.top = i * 100 + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
+    for(let i = 0; i < getQuantityElements(100 * settings.traffic); i++) {
+        const enemy = document.createElement(`div`);
+        enemy.classList.add(`enemy`);
+        enemy.y = -100 * settings.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + `px`;
+        enemy.style.top = enemy.y + `px`;
+        enemy.style.background = 'transparent url("./image/enemy2.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+
+    }
+
+    settings.start = true;
+    settings.score = 0;
+    gameArea.appendChild(car);
+    car.style.left = (gameArea.offsetWidth / 2 - car.offsetWidth / 2) + `px`;
+    car.style.top = `auto`;
+    car.style.bottom = `10px`;
+    settings.x = car.offsetLeft;
+    settings.y = car.offsetTop;
+    requestAnimationFrame(playGame);
+}
+
+function playGame() {
+
+    if (settings.start) {
+
+        settings.score+= settings.speed;
+        score.textContent = settings.score;
+        moveRoad();
+        moveEnemy();
+
+        if (keys.ArrowLeft && settings.x > 0) {
+            settings.x -= settings.speed;
+        } else if (keys.ArrowRight && settings.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            settings.x += settings.speed;
+        } else if (keys.ArrowDown && settings.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            settings.y += settings.speed;
+        } else if (keys.ArrowUp && settings.y > 0) {
+            settings.y -= settings.speed;
+        }
+
+        car.style.left = settings.x + `px`;
+        car.style.top = settings.y + `px`;
+        requestAnimationFrame(playGame);
+    }   
+}
+
+function startRun(evt) {
+    evt.preventDefault();
+    keys[evt.key] = true;
+}
+
+function stopRun(evt) {
+    evt.preventDefault();
+    keys[evt.key] = false;
+}
+
+function moveRoad() {
+    let lines = document.querySelectorAll(`.line`);
+
+    lines.forEach(function(line) {
+        line.y += settings.speed;
+        line.style.top = line.y + `px`;
+
+        if (line.y >= document.documentElement.clientHeight) {
+            line.y = -100;
+        }
+    });
+}
+
+function moveEnemy() {
+    let enemy = document.querySelectorAll(`.enemy`);
+
+    enemy.forEach(function(it) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = it.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+                settings.start = false;
+                start.classList.remove(`hide`);
+                start.style.top = score.offsetHeight + `px`;
+        }
+
+        it.y += settings.speed / 2;
+        it.style.top = it.y + `px`;
+
+        if (it.y >= document.documentElement.clientHeight) {
+            it.y = -100 * settings.traffic;
+            it.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + `px`;
+        }
+    });
+}
